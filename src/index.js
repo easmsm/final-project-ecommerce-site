c287c0361f3493916cc7af104d6d2e709cea6436
ekukwa <easmsm@gmail.com>
2022-10-24 18:23:21 -0400

    error resolved, button class was causing it

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 
-var element = <button>Hello World!</div>;
+var element = <div>Hello World!</div>;
 ReactDom.render(element, document.getElementById("root"));
