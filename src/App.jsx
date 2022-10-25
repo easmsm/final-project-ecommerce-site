4b16c11e5ac4d900be99988c52d14a5704ae9155
ekukwa <easmsm@gmail.com>
2022-10-24 20:03:21 -0400

    added app.jsx, maincontent.jsx, and navbar.jsx as separate files in src, all referenced by App.jsx which will be the main file for src

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,13 @@
 import React, { Component } from "react";
-class NavBar extends Component{
-    render(){
-        return (
-        <React.Fragment>
-            <nav class="navbar navbar-expand-lg bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Ecommerce App Placeholder Name</a>
-                    
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+import NavBar from "./NavBar";
+import MainContent from "./MainContent";
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Services</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Products</a>
-                    </li>
-
-                    {/* <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-                 
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li> */}
-
-                    {/* <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
-                    </li> */}
-                </ul>
-
-                     {/* <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>  */}
-                </div>
-            </div>
-        </nav>
-        </React.Fragment>
+export default class App extends Component {
+    render() {
+        return (<React.Fragment>
+                <NavBar />
+                <MainContent />
+            </React.Fragment>
         );
     }
 }
-
-export default NavBar;
---
+++ src/MainContent.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export default class MainContent 
+extends Component
+{
+    render()
+    {
+        return<div>Here's some information about our ecommerce company</div>;
+    }
+}
