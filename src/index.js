3cf94938affaa87b8fe4d56cd8d7407dbdeddad4
Shanisse <user@user1smacbook2.myfiosgateway.com>
2022-10-29 15:52:07 -0400

    installed bootsrap and added components

--- src/index.js
+++ src/index.js
@@ -4,8 +4,23 @@ import "jquery";
 import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
-import App from "./App";
-import "./index.css";
 import "font-awesome/css/font-awesome.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+reportWebVitals();
 
 ReactDom.render(<App />, document.getElementById("root"));
